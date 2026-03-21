7047b3f9992dd0a9ded275e6cd7858df199a7247
Thabata Ganga <60260735+thabataganga@users.noreply.github.com>
2026-03-20 21:23:13 -0300

    mudar nome do repositorio

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.GITHUB_ACTIONS ? "/limbse-website" : "",
+  basePath: "",
 };
 
 export default nextConfig;
